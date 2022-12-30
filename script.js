4ebac4cfc5b8253d00bf658c72e4ad2f98daaba3
abos-gergo <gergo.abos@gmail.com>
2022-12-30 11:58:22 +0100

    Squared canvas

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 var background;
 var player1;
+var player2;
 
 function startGame() {
     background = new GameObject(720, 720, "Assets/Background.png", 0, 0, "image");
-    //player1 = new GameObject(30, 30, "green", 10, 120);
+    player1 = new GameObject(30, 30, "green", 10, 120);
+    player2 = new GameObject(100, 100, "red", 0, 620);
     myGameArea.start();
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 1260;
+        this.canvas.width = 720;
         this.canvas.height = 720;
         this.context = this.canvas.getContext("2d");
+        this.context.imageSmoothingEnabled = false;
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
     },
@@ -47,5 +50,6 @@ function GameObject(width, height, color, x, y, type) {
 function updateGameArea() {
     myGameArea.clear();
     background.update();
-    //player1.update();
+    player1.update();
+    player2.update();
 }
