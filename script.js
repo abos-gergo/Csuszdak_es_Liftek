eecb332771e7d4e02f7efc5c4b2aaff4f352acf7
norbert-gemes <gemes.norbert@students.jedlik.eu>
2023-01-07 13:28:54 +0100

    Menu Játékos szám-Gombok műkődése(részleges)

--- script.js
+++ script.js
@@ -1,55 +1,86 @@
 var background;
 var player1;
 var player2;
+var number = 1;
+var text = document.querySelector("#number");
+text.innerHTML = `${number}db játékos`;
+var decreaseButton = document.querySelector("#minus");
+decreaseButton.innerHTML = "Csökkentés";
+var increaseButton = document.querySelector("#plus");
+increaseButton.innerHTML = "Növelés";
+var startButton = document.querySelector("#start");
+startButton.innerHTML = "Játék indítása!";
 
 function startGame() {
-    background = new GameObject(720, 720, "Assets/Background.png", 0, 0, "image");
-    player1 = new GameObject(30, 30, "green", 10, 120);
-    player2 = new GameObject(100, 100, "red", 0, 620);
-    myGameArea.start();
+  background = new GameObject(720, 720, "Assets/Background.png", 0, 0, "image");
+  player1 = new GameObject(30, 30, "green", 10, 120);
+  player2 = new GameObject(100, 100, "red", 0, 620);
+  myGameArea.start();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 720;
-        this.canvas.height = 720;
-        this.context = this.canvas.getContext("2d");
-        this.context.imageSmoothingEnabled = false;
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 720;
+    this.canvas.height = 720;
+    this.context = this.canvas.getContext("2d");
+    this.context.imageSmoothingEnabled = false;
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+};
 
 function GameObject(width, height, color, x, y, type) {
-    if (type == "image") {
-        this.img = new Image();
-        this.img.src = color;
-    }
-    this.type = type;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
+  if (type == "image") {
+    this.img = new Image();
+    this.img.src = color;
+  }
+  this.type = type;
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
 
-    this.update = function(){
-        ctx = myGameArea.context;
-        if (this.type == "image") {
-            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        }
-        else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
+  this.update = function () {
+    ctx = myGameArea.context;
+    if (this.type == "image") {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    } else {
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+  };
 }
 
 function updateGameArea() {
-    myGameArea.clear();
-    background.update();
-    player1.update();
-    player2.update();
+  myGameArea.clear();
+  background.update();
+  player1.update();
+  player2.update();
 }
+
+function DecreasePlayer() {
+  if (number > 1 && number <= 4) {
+    number = number - 1;
+    text.innerHTML = `${number}db játékos`;
+  }
+}
+
+function IncreasePlayer() {
+  if (number >= 1 && number < 4) {
+    number = number + 1;
+    text.innerHTML = `${number}db játékos`;
+  }
+}
+
+function GeneratePlayers() {
+  decreaseButton.disabled = true;
+  increaseButton.disabled = true;
+}
+
+decreaseButton.addEventListener("click", DecreasePlayer);
+increaseButton.addEventListener("click", IncreasePlayer);
+startButton.addEventListener("click", GeneratePlayers);
